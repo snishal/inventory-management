a4448495bfa0dc9cdbde527596b34fa9ef3c1800
sahilnishal <snishal33@gmail.com>
2021-01-29 09:06:36 +0530

    Add JsonIgnoreProperties

--- src/main/java/com/paytm/inventorymanagement/models/Inventory.java
+++ src/main/java/com/paytm/inventorymanagement/models/Inventory.java
@@ -1,11 +1,14 @@
 package com.paytm.inventorymanagement.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity(name = "inventories")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Inventory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
