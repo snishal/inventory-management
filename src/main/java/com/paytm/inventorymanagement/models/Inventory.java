57df5bc5ccb8035a873aa06c1a653411c85131f5
sahilnishal <snishal33@gmail.com>
2021-02-02 10:34:54 +0530

    Drop lazyloader from JsonIgnore

--- src/main/java/com/paytm/inventorymanagement/models/Inventory.java
+++ src/main/java/com/paytm/inventorymanagement/models/Inventory.java
@@ -5,15 +5,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity(name = "inventories")
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Getter @Setter @NoArgsConstructor
-public class Inventory {
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter @Setter
+public class Inventory{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
